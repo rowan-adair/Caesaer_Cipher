6def35b380fa1b061fd275cc71e43bebd342e5b6
The-Boat <radair2842@gmail.com>
2019-10-23 09:42:35 +0100

    Added decryption and decryption test

--- Caesaer_Cipher/Main.cpp
+++ Caesaer_Cipher/Main.cpp
@@ -5,42 +5,55 @@
 using namespace std;
 
 
-
-string encrypt_message(string message, int setting)
-{
-	char alphabet_arr[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char* alphabet = alphabet_arr;
-	
-	
-
+bool valid_message(string message, int setting) {
 	if (message.empty() == true) {
-		return "Invalid message!";
+		return false;
 	}
 	else if (setting == 0) {
-		return "You haven't selected a setting";
+		return false;
 	}
+	return true;
+}
 
+
+string Caesaer_Cipher(string message, int setting, bool decrypt) {
+	if (!valid_message(message, setting)) return "Invalid Parameters";
+	char alphabet_arr[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	for (int t = 0; t < message.size(); t++) {
 		for (int i = 0; i < 26; i++) {
 
 			if (isspace(message[t])) break;
 
-			if (message[t] == alphabet[i]) {
-				message[t] = alphabet[i + setting];
+			if (message[t] == alphabet_arr[i]) {
+				if(decrypt)
+					message[t] = alphabet_arr[i - setting];
+				else					
+					message[t] = alphabet_arr[i + setting];
 				break;
 			}
 			if (i == 25) i = 0;
 		}
 	}
-
 	return message;
 }
+
+
 
 // Test
 int main() {
 	
-	string test_message = "attack";
+	string test_encrypt_message = "attack";
+	string test_encrypted_message = Caesaer_Cipher(test_encrypt_message, 5, false);
+	cout << "Before encryption : " << test_encrypt_message << endl; // Expected output attack
+	cout << "After encryption :" << test_encrypted_message << endl; // Expected output fyyfhp
 
-	cout <<encrypt_message(test_message,5) << endl; // Expected output fyyfhp
+	cout << "------------------------------" << endl;
+
+	string test_decrypt = Caesaer_Cipher(test_encrypted_message, 5, true);
+
+	cout << "Before decryption :" << test_encrypted_message << endl; // Expected output fyyfhp
+	cout << "After decryption :" << test_decrypt << endl; // Expected output attack
+
+
 	return 0;
 }
