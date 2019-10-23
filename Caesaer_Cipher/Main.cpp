4f3383b0ae80d83e30fe3662fb9ce9c3649f6e7e
The-Boat <radair2842@gmail.com>
2019-10-23 10:51:25 +0100

    Added encrpyt/decrypt console interface. Still need to add capital support

--- Caesaer_Cipher/Main.cpp
+++ Caesaer_Cipher/Main.cpp
@@ -1,4 +1,3 @@
-#include "Cipher.cpp"
 #include <iostream>
 #include <string>
 
@@ -23,7 +22,7 @@ string Caesaer_Cipher(string message, int setting, bool decrypt) {
 		for (int i = 0; i < 26; i++) {
 
 			if (isspace(message[t])) break;
-
+			//message[t] = putchar(tolower(message[t]));
 			if (message[t] == alphabet_arr[i]) {
 				if(decrypt)
 					message[t] = alphabet_arr[i - setting];
@@ -37,10 +36,46 @@ string Caesaer_Cipher(string message, int setting, bool decrypt) {
 	return message;
 }
 
+bool is_decrypt(int choice) {
+	// 
+	if (choice == 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+void start() {
 
-// Test
-int main() {
+	int choice = 0;
+	int setting = 1;
+	string message = "";
+	cout << "Do you want to encrypt or decrypt a message? (0 = Encrypt, 1 = Decrypt)" << endl;
+	cin >> choice;
+	
+	bool decrypt = is_decrypt(choice);
+
+	if (decrypt){
+		cout << "Enter a message to decrypt" << endl;
+	}
+	else {
+		cout << "Enter a message to encrypt" << endl;
+	}
+	
+	cin >> message;
+
+	cout << "Choose an integer setting" << endl;
+
+	cin >> setting;
+
+	string result = Caesaer_Cipher(message, setting, decrypt);
+
+	cout <<  result << endl;
+
+}
+
+int Test() {
 	
 	string test_encrypt_message = "attack";
 	string test_encrypted_message = Caesaer_Cipher(test_encrypt_message, 5, false);
@@ -55,5 +90,21 @@ int main() {
 	cout << "After decryption :" << test_decrypt << endl; // Expected output attack
 
 
+	return 0;
+}
+
+int main() {
+
+	start();
+	
+	char run_again;
+
+	cout << "Would you like to encrypt or decrypt another message? (y/n)" << endl;
+	cin >> run_again;
+
+	if (run_again == 'y') {
+		start();
+	}
+	
 	return 0;
 }
